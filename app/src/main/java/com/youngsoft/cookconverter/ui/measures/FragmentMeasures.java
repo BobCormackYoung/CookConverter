1d6a6f7c02b44817b5f56d4c4dc101dcf6d19d94
Pauhoihoi <rob.cormack@gmail.com>
2020-05-24 23:12:49 +0200

    input to output conversion logic

--- app/src/main/java/com/youngsoft/cookconverter/ui/measures/FragmentMeasures.java
+++ app/src/main/java/com/youngsoft/cookconverter/ui/measures/FragmentMeasures.java
@@ -1,9 +1,13 @@
 package com.youngsoft.cookconverter.ui.measures;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,26 +16,101 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.youngsoft.cookconverter.R;
+
+import org.w3c.dom.Text;
 
 public class FragmentMeasures extends Fragment {
 
     private ViewModelMeasures viewModelMeasures;
+    private Spinner spInput;
+    private Spinner spOutput;
+    private TextInputEditText etInputValue;
+    private TextInputEditText etOutputValue;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         viewModelMeasures = ViewModelProviders.of(this).get(ViewModelMeasures.class);
         View root = inflater.inflate(R.layout.fragment_measures, container, false);
+        mapViews(root); //map views for the root layout
+        return root;
+    }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setListeners(); //init view listeners
+        setObservers(); //init viewmodel observers
+        etInputValue.setText("0.0"); //TODO: can this be removed?
+    }
 
-
-        viewModelMeasures.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+    /**
+     * set listeners on the view fields & update the viewmodel when changed
+     */
+    private void setListeners() {
+        //listen for changes to the input value edit text field
+        etInputValue.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onChanged(@Nullable String s) {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (etInputValue.getText().toString().isEmpty()) {
+                    viewModelMeasures.setInputValueMutable(0.0);
+                } else {
+                    viewModelMeasures.setInputValueMutable(Double.valueOf(etInputValue.getText().toString()));
+                }
+                Log.i("FM","input text changed");
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    /**
+     * set observers for the viewmodel and update view when changed
+     */
+    private void setObservers() {
+
+        //observe changes to the output value
+        viewModelMeasures.getMediatorOutput().observe(getViewLifecycleOwner(), new Observer<Double>() {
+            @Override
+            public void onChanged(Double aDouble) {
+                etOutputValue.setText(aDouble.toString());
+                Log.i("FM","input value updated");
             }
         });
 
-
-        return root;
+        /**
+        //observe changes to the input value
+        viewModelMeasures.getInputValue().observe(getViewLifecycleOwner(), new Observer<Double>() {
+            @Override
+            public void onChanged(Double aDouble) {
+                //if not the same as the existing text, update the text field
+                if (aDouble.toString().equals(etInputValue.getText().toString())) {
+                    Log.i("FM","input value the same: " + aDouble+ " vs " + Double.valueOf(etInputValue.getText().toString()) );
+                } else {
+                    etInputValue.setText(aDouble.toString());
+                    Log.i("FM","input value updated: " + aDouble+ " vs " + Double.valueOf(etInputValue.getText().toString()) );
+                }
+            }
+        });**/
     }
+
+    /**
+     * map views from the root view
+     * @param root
+     */
+    private void mapViews(View root) {
+        spInput = root.findViewById(R.id.sp_measure_input);
+        spOutput = root.findViewById(R.id.sp_measure_output);
+        etInputValue = root.findViewById(R.id.tiet_measures_input);
+        etOutputValue = root.findViewById(R.id.tiet_measures_output);
+    }
+
 }
--- app/src/main/java/com/youngsoft/cookconverter/ui/measures/ViewModelMeasures.java
+++ app/src/main/java/com/youngsoft/cookconverter/ui/measures/ViewModelMeasures.java
@@ -1,19 +1,73 @@
 package com.youngsoft.cookconverter.ui.measures;
 
+import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 
-public class ViewModelMeasures extends ViewModel {
+import com.youngsoft.cookconverter.data.DataRepository;
 
-    private MutableLiveData<String> mText;
+public class ViewModelMeasures extends AndroidViewModel {
 
-    public ViewModelMeasures() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+    private DataRepository dataRepository;
+
+    private MutableLiveData<Double> inputValue;
+    private MutableLiveData<Double> outputValue;
+    private MutableLiveData<Double> conversionFactor;
+    private MediatorLiveData<Double> mediatorOutput;
+
+    public ViewModelMeasures(@NonNull Application application) {
+        super(application);
+        inputValue = new MutableLiveData<>();
+        inputValue.setValue(0.0);
+        outputValue = new MutableLiveData<>();
+        outputValue.setValue(0.0);
+        conversionFactor = new MutableLiveData<>();
+        conversionFactor.setValue(2.0);
+
+        //create mediator output to reactively convert when changing either input value or conversion value
+        mediatorOutput = new MediatorLiveData<Double>();
+        //observe the input value
+        mediatorOutput.addSource(inputValue, new Observer<Double>() {
+            @Override
+            public void onChanged(Double aDouble) {
+                if (inputValue.getValue() != null && conversionFactor.getValue() != null) {
+                    mediatorOutput.setValue(inputValue.getValue()*conversionFactor.getValue());
+                }
+            }
+        });
+        //observe the conversion factor value
+        mediatorOutput.addSource(conversionFactor, new Observer<Double>() {
+            @Override
+            public void onChanged(Double aDouble) {
+                if (inputValue.getValue() != null && conversionFactor.getValue() != null) {
+                    mediatorOutput.setValue(inputValue.getValue()*conversionFactor.getValue());
+                }
+            }
+        });
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<Double> getInputValue() {
+        return inputValue;
+    }
+
+    public LiveData<Double> getOutputValue() {
+        return outputValue;
+    }
+
+    //set the input value
+    public void setInputValueMutable(Double input) {
+        inputValue.setValue(input);
+    }
+
+    //set the converted value
+    public LiveData<Double> getMediatorOutput() {
+        return mediatorOutput;
     }
 }
